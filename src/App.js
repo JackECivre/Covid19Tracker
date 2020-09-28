715980b55147fea16ade1dac879aa6cf419e1d65
JackE <jacke@kavanu.co>
2020-09-29 01:49:45 +0300

    Data retrieved from api and displayed as cards before styling

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 
-
 class App extends React.Component {
+  state = {
+    data: {},
+  };
 
   async componentDidMount() {
-    const data = await fetchData();
+    const fetchedData = await fetchData();
 
-    console.log(data)
-
+    this.setState({ data: fetchedData });
   }
 
   render() {
+    const { data } = this.state;
+
     return (
-      <div className= {styles.container}>
-        <Cards />
-        <Chart />
+      <div className={styles.container}>
+        <Cards data={data} />
         <CountryPicker />
+        <Chart />
       </div>
     );
   }
--- src/api/index.js
+++ src/api/index.js
@@ -14,5 +14,6 @@ export const fetchData = async () => {
       deaths,
       lastUpdate,
     };
+    
   } catch (error) {}
 };
